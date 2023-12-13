65775e0e627bf6cfbd89179010fd70d7ea4f41f6
Jamshidbek Codes <jamshidbekmirzakarimov6@gmail.com.>
2023-12-13 12:00:25 +0500

    fixed

--- src/Modules/Header/Header.jsx
+++ src/Modules/Header/Header.jsx
@@ -126,7 +126,7 @@ const Header = () => {
 
   return (
     <motion.div initial="initial" animate="animate">
-      <motion.header variants={stagger} className="py-[16px] h-[10vh] bg-white">
+      <motion.header variants={stagger} className="py-[16px] h-[10vh] bg-white flex items-center justify-center">
         <div className="container">
           <div className="flex items-center justify-between">
             <div className="flex items-center">
